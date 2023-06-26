7d8355f4e149c9e9475f0fd4c5ff8f5c3ddeb240
Dmitriy Radionov <dmitriy.radionov@gmail.com>
2023-06-26 16:38:58 +0300

    Update discription in package 'generic'

--- src/generics/game/Team.java
+++ src/generics/game/Team.java
@@ -17,6 +17,14 @@ import java.util.Random;
     <?> - любой класс
     <? extends X> - класс X или любой его сабкласс
     <? super Y> - класс Y или любой его суперкласс
+    --------------------------------------------------------------------
+    The most commonly used type parameter names are:
+        E - Element (used extensively by the Java Collections Framework)
+        K - Key
+        N - Number
+        T - Type
+        V - Value
+        S, U, V etc. - 2nd, 3rd, 4th types
 */
 
     // 0
